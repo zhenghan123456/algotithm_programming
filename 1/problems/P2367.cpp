651c413f99075988796c1b57774f6eb042a5b6f1
zhenghan123456 <15394270057@163.com>
2026-08-02 07:03:27 +0800

    admin upload /1/problems/P2367.cpp

--- 1/problems/P2367.cpp
+++ 1/problems/P2367.cpp
@@ -19,21 +19,23 @@ int main()
     d[0] = a[0];
     _rep(i, 1, n)
     {
-        d[i] = a[i] + a[i - 1];
+        d[i] = a[i] - a[i - 1];
     }
     _for(i, p)
     {
         int l, r, v;
         cin >> l >> r >> v;
+        // 转为0_based
         l--;
         r--;
         d[l] += v;
         d[r + 1] -= v;
     }
-    _rep(i, 1, n) d[i] += d[i - 1];
+    a[0] = d[0];
+    _rep(i, 1, n) a[i] = d[i] += d[i - 1]; // 当然这里也可以对差分数组原地操作，不过这样语义更明确
     int minn = INT_MAX;
     // 求最小值
-    _for(i, n) if (minn > d[i]) minn = d[i];
+    _for(i, n) if (minn > a[i]) minn = a[i];
 
     cout << minn << endl;
 }
