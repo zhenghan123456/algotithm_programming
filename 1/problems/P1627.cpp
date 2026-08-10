aa5fa984bb48d2ed76c62d06c1eab2547b0b0c61
zhenghan123456 <15394270057@163.com>
2026-08-10 18:24:45 +0800

    update files

--- 1/problems/P1627.cpp
+++ 1/problems/P1627.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 typedef long long ll;
 const int maxn = 2e5 + 5;
-// c[0][s]：未遇到b时，前缀和为s的出现次数
-// c[1][s]：已经遇到过b之后，前缀和为s的出现次数
-ll c[2][maxn]; // 题目未明确数据范围，用ll更稳妥
+ll cnt_left[maxn * 2], cnt_right[maxn * 2];
 #define _for(i, n) for (int i = 0; i < n; i++)
 #define _rep(i, a, b) for (int i = a; i < b; i++)
 #define endl '\n'
@@ -14,30 +12,44 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n;
-    ll b, ans;
+    ll b;
     cin >> n >> b;
-
-    c[0][n] = 1;
-    // flag标记：是否已经读取到数值b，0未遇到，1已遇到
-    bool flag = 0;
-    // s为偏移后的前缀和：大于b记+1，小于b记‑1；初始偏移n防止下标为负
-    int s = n;
-
-    for (int i = 0; i < n; i++)
+    vector<ll> arr(n);
+    int pos = -1; // 记录第一个b出现的下标
+    _for(i, n)
     {
-        ll a;
-        cin >> a;
-        // 当前数不等于b：更新偏移前缀和
-        if (a != b)
-            s += a > b ? 1 : -1;
-        c[flag |= (a == b)][s]++; // 一旦碰到b，flag永久置1；
+        cin >> arr[i];
+        if (pos == -1 && arr[i] == b)
+            pos = i;
     }
 
-    ans = 0;
-    // 注意：合法子数组要求子数组包含b，
-    for (int i = 0; i < 2 * n; i++)
+    int offset = n;
+    // 第一步：统计b左侧所有前缀和（0 ~ pos-1）
+    int sum = offset;
+    cnt_left[sum] = 1;
+    _for(i, pos)
     {
-        ans += c[0][i] * c[1][i];
+        if (arr[i] > b)
+            sum++;
+        else if (arr[i] < b)
+            sum--;
+        cnt_left[sum]++;
+    }
+
+    // 第二步：统计b及右侧所有前缀和（pos ~ n-1）
+    _rep(i, pos, n)
+    {
+        if (arr[i] > b)
+            sum++;
+        else if (arr[i] < b)
+            sum--;
+        cnt_right[sum]++;
+    }
+
+    ll ans = 0;
+    _rep(i, 0, 2 * n + 1)
+    {
+        ans += cnt_left[i] * cnt_right[i];
     }
     cout << ans << endl;
     return 0;
